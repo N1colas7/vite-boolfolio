43b51f6ebc041f0df72eae1bb1adf797ae970fc4
N1colas7 <nicolasnd9897@gmail.com>
2023-03-13 16:38:20 +0100

    install bootstrap

---
+++ src/components/PostMain.vue
@@ -0,0 +1,38 @@
+<script>
+export default {
+    name: "PostMain",
+    data() {
+        return {
+            posts: [],
+            loading: true,
+            baseUrl: 'http:/127.0.0.1:8000'
+        }
+    },
+    methods: {
+        getPosts() {
+            this.loading = true;
+            axios.get(`${this.baseUrl}/api/posts`).then((response) => {
+                if (response.data.success) {
+                    this.posts = response.data.posts
+                    this.loading = false;
+                }
+                else {
+                    //compare messaggio che non è andata a buon fine la chiamata
+
+                }
+            });
+        }
+    },
+    mounted() {
+        this.getPosts()
+    },
+}
+</script>
+<template lang="">
+    <div>
+        <button class="btn btn-primary">ciao</button>
+    </div>
+</template>
+<style lang="scss" scoped>
+
+</style>
