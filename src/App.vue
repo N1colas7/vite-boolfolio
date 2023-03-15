78423b96d5f02119d5dd0cbdf11402b3b6118516
N1colas7 <nicolasnd9897@gmail.com>
2023-03-15 17:26:30 +0100

    router.js done

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,14 @@
 <script>
-import PostMain from './components/PostMain.vue'
 
 export default{
   components:{
-    PostMain
   }
 }
 
 </script>
 
 <template>
-  <PostMain></PostMain>
+  <router-view></router-view>
 
 </template>
 
