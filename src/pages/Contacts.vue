232828c3b4c5f79e75cc8a50974be4823dc546bc
N1colas7 <nicolasnd9897@gmail.com>
2023-03-16 17:52:19 +0100

    done form for contacs and call api done

--- src/pages/Contacts.vue
+++ src/pages/Contacts.vue
@@ -1,17 +1,38 @@
 <script>
+    import axios from 'axios';
 export default {
+
     name: 'Contacts',
     data() {
         return {
-            
+            name:'',
+            surname: '',
+            email: '',
+            phone:'',
+            message:'',
         }
     },
+    methods:{
+        sendForm(){
+            const data = {
+                name: this.name,
+                surname: this.surname,
+                email: this.email,
+                phone: this.phone,
+                message: this.message,                
+            }
+
+            axios.post(`${this.store.baseUrl}/api/contacts`,data).then((response) => {
+
+            });
+        }
+    }
 }
 </script>
 <template lang="">
     <div class="container">
         <div class="row my-5">
-            <div class="col-12">
+            <div class="col-12 ">
                 <h2 class="text-center my-5">Pagina dei Contatti</h2>
             </div>
             <div class="col-12">
@@ -50,14 +71,59 @@ export default {
                         </div>
                     </div>
                 </div>
-                <div class="col-12 my-5">
+                <div class="col-12 my-5 write-on">
                     <h5 class="text-center">Scrivici</h5>
-                    <form></form>
+                    <form @submit="sendForm">
+                        <div class="row">
+                            <div class="row">
+                                <div class="col-12 col-md-6 my-2">
+                                    <label for="nome" class="control-label">Nome</label>
+                                    <input type="text" class="form-control" name="nome" id="nome" placeholder="Nome" v-model="name">
+                                </div>
+                                <div class="col-12 col-md-6 my-2">
+                                    <label for="cognome" class="control-label">Cognome</label>
+                                    <input type="text" class="form-control" name="cognome" id="cognome" placeholder="Cognome" v-model="surname">
+                                </div>
+                                <div class="col-12 col-md-6 my-2">
+                                    <label for="email" class="control-label">Email</label>
+                                    <input type="mail" class="form-control" name="email" id="email" placeholder="Email" v-model="email">
+                                </div>
+                                <div class="col-12 col-md-6 my-2">
+                                    <label for="telefono" class="control-label">Telefono</label>
+                                    <input type="phone" class="form-control" name="telefono" id="telefono" placeholder="Telefono" v-model="phone">
+                                </div>
+                                <div class="col-12 my-2">
+                                    <label class="control-label" for="messaggio">Messaggio</label>
+                                    <textarea class="form-control" name="messaggio" id="messaggio" placeholder="Messaggio" v-model="message">
+                                    </textarea>
+                                </div>
+                                <div class="col-12 my-2 text-center">
+                                    <button type="submit" class="send_email">Invia</button>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
     </div>
 </template>
-<style lang="">
-    
+<style lang="scss" scoped>
+    .write-on{
+        background-color: rgb(160, 116, 95);
+        color: white;
+        border-radius: 10px;
+    }
+     .send_email{
+        background-color: white;
+        color:rgb(160, 116, 95);
+        border: none;
+        padding: 10px 20px;
+        transi
+
+        &:hover{
+            background-color: rgb(116, 84, 69);
+            color: white;
+        }
+    }
 </style>
